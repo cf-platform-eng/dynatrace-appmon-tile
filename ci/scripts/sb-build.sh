690afe7f6831b1a3988d65f6bbf15db8fe6f4b4f
Sabha Parameswaran <sabhap@pivotal.io>
2016-04-12 15:47:49 -0700

    Fix sb build script

--- ci/scripts/sb-build.sh
+++ ci/scripts/sb-build.sh
@@ -3,9 +3,6 @@
 SOURCE_DIR=$1
 TARGET_DIR=$1
 
-(cd ${SOURCE_DIR}; mvn -Dmaven.test.skip=true install)
-
-VERSION=`grep '^version:' ${SOURCE_DIR}/tile-history.yml | sed 's/^version: //'`
-HISTORY="tile-history-${VERSION}.yml"
-
+cd ${SOURCE_DIR} 
+mvn -Dmaven.test.skip=true install
 cp ${SOURCE_DIR}/target/*.jar ${TARGET_DIR}
