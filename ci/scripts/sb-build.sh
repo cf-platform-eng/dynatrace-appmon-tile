101eadcf8766e334157c59bdea53ea77948d9b9a
Sabha Parameswaran <sabhap@pivotal.io>
2016-04-14 22:09:08 -0700

    Update ci scripts and add timeout for app push

--- ci/scripts/sb-build.sh
+++ ci/scripts/sb-build.sh
@@ -1,8 +1,11 @@
 #!/bin/sh -exv
 
+# Get absolute path and not relative path
 SOURCE_DIR="$(cd "$1" && pwd)"
 TARGET_DIR="$(cd "$2" && pwd)"
 
 cd ${SOURCE_DIR} 
 mvn -Dmaven.test.skip=true install
+
+# This would fail with relative paths
 cp ${SOURCE_DIR}/target/*.jar ${TARGET_DIR}
